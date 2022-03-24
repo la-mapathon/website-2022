010be4710d3d26f761859a97c79b5475d0d0a29d
Nina Kin <matikin9@gmail.com>
2022-03-24 11:10:29 -0700

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,8 @@
 module.exports = function(eleventyConfig) {
 
-	eleventyConfig.addPassthroughCopy("assets");
+	eleventyConfig.addPassthroughCopy("images");
 	eleventyConfig.addPassthroughCopy("css");
+	eleventyConfig.addPassthroughCopy("js");
 
 	return {
 		dir: {
