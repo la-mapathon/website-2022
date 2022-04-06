1a7ee91a5584f16cf1e0968724d93d12607ee217
Nina Kin <matikin9@gmail.com>
2022-04-06 00:05:11 -0700

    add data file for team page

---
+++ src/_data/team.js
@@ -0,0 +1,40 @@
+module.exports = {
+	name: "Team Members",
+	members: [
+		{
+			name: "Andy Rutkowski",
+			blurb: "Ask me about cartography!",
+			image: "../images/andy.png"
+		},
+		{
+			name: "Elizabeth  Sedano",
+			blurb: "Ask me about OSM!",
+			image: "../images/lisa.png"
+		},
+		{
+			name: "Laura Loyola",
+			blurb: "Ask me about environmental GIS!",
+			image: "../images/laura.png"
+		},
+		{
+			name: "Yoh Kawano",
+			blurb: "Ask me about spatial data science!",
+			image: "../images/yoh.png"
+		},
+		{
+			name: "Zoe Borovsky",
+			blurb: "Ask me about OSM!",
+			image: "../images/zoe.png"
+		},
+		{
+			name: "James Walker",
+			blurb: "Ask me about Humanitarian GIS!",
+			image: "../images/james.png"
+		},
+		{
+			name: "Heather Chou",
+			blurb: "Ask me about Humanitarian GIS!",
+			image: "https://uclageographyassociation268883444.files.wordpress.com/2020/11/img_6256-1.jpg?w=300"
+		}
+	]
+};
