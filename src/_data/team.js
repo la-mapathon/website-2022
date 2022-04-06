d7c0b84fd0507e626cdfe053f665e0a982b1749e
yohman <yohman@gmail.com>
2022-04-06 10:43:11 -0700

    team update

--- src/_data/team.js
+++ src/_data/team.js
@@ -4,37 +4,66 @@ module.exports = {
 		{
 			name: "Andy Rutkowski",
 			blurb: "Ask me about cartography!",
-			image: "../images/andy.png"
+			image: "../images/andy.png",
+			affiliation: "USC",
+			status: "Staff",
+			org: "Library"
 		},
 		{
 			name: "Elizabeth  Sedano",
 			blurb: "Ask me about OSM!",
-			image: "../images/lisa.png"
+			image: "../images/lisa.png",
+			affiliation: "USC",
+			status: "Faculty",
+			org: ""
 		},
 		{
 			name: "Laura Loyola",
 			blurb: "Ask me about environmental GIS!",
-			image: "../images/laura.png"
+			image: "../images/laura.png",
+			affiliation: "USC",
+			status: "Staff",
+			org: ""
 		},
 		{
 			name: "Yoh Kawano",
 			blurb: "Ask me about spatial data science!",
-			image: "../images/yoh.png"
+			image: "../images/yoh.png",
+			affiliation: "UCLA",
+			status: "Staff",
+			org: "Office of Advanced Research Computing"
 		},
 		{
 			name: "Zoe Borovsky",
 			blurb: "Ask me about OSM!",
-			image: "../images/zoe.png"
+			image: "../images/zoe.png",
+			affiliation: "UCLA",
+			status: "Staff",
+			org: "Library"
 		},
 		{
 			name: "James Walker",
 			blurb: "Ask me about Humanitarian GIS!",
-			image: "../images/james.png"
+			image: "../images/james.png",
+			affiliation: "UCLA",
+			status: "Staff",
+			org: ""
+		},
+		{
+			name: "Zhiyuan Yao",
+			blurb: "Ask me about QGIS!",
+			image: "../images/zhiyuan.png",
+			affiliation: "UCLA",
+			status: "Staff",
+			org: "Data Science Center"
 		},
 		{
 			name: "Heather Chou",
 			blurb: "Ask me about Humanitarian GIS!",
-			image: "https://uclageographyassociation268883444.files.wordpress.com/2020/11/img_6256-1.jpg?w=300"
+			image: "https://uclageographyassociation268883444.files.wordpress.com/2020/11/img_6256-1.jpg?w=300",
+			affiliation: "UCLA",
+			status: "Student",
+			org: "Geographer's Association"
 		}
 	]
 };
