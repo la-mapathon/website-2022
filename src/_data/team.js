72abaf0da10ed6420ae93e97cd29dd5f8d4c55bd
yohman <yohman@gmail.com>
2022-04-15 15:29:38 -0700

    more members!

--- src/_data/team.js
+++ src/_data/team.js
@@ -2,6 +2,38 @@ module.exports = {
 	name: "Team Members",
 	members: [
 		{
+			name: "Heather Chou",
+			blurb: "Ask me about the UCLA Geographer's Association!",
+			image: "https://uclageographyassociation268883444.files.wordpress.com/2020/11/img_6256-1.jpg?w=300",
+			affiliation: "UCLA",
+			status: "Student",
+			org: "Geographer's Association"
+		},
+		{
+			name: "Cerys Edwards",
+			blurb: "Ask me about the UCLA Library's map collection!",
+			image: "../images/cerys.png",
+			affiliation: "UCLA",
+			status: "Student",
+			org: "UCLA Library"
+		},
+		{
+			name: "Will Foote",
+			blurb: "Ask me about statistics (and all things Wordle)!",
+			image: "../images/will.png",
+			affiliation: "UCLA",
+			status: "Student",
+			org: "UCLA Library"
+		},
+		{
+			name: "Will Forker, Sophie Beck, Eva Shafman",
+			blurb: "Ask us about SC Mappers!",
+			image: "../images/will.jpeg",
+			affiliation: "USC",
+			status: "Student",
+			org: "SC Mappers"
+		},
+		{
 			name: "Andy Rutkowski",
 			blurb: "Ask me about cartography!",
 			image: "../images/andy.png",
@@ -56,30 +88,6 @@ module.exports = {
 			affiliation: "UCLA",
 			status: "Staff",
 			org: "Data Science Center"
-		},
-		{
-			name: "Heather Chou",
-			blurb: "Ask me about the UCLA Geographer's Association!",
-			image: "https://uclageographyassociation268883444.files.wordpress.com/2020/11/img_6256-1.jpg?w=300",
-			affiliation: "UCLA",
-			status: "Student",
-			org: "Geographer's Association"
-		},
-		{
-			name: "Cerys Edwards",
-			blurb: "Ask me about the UCLA Library's map collection!",
-			image: "../images/cerys.png",
-			affiliation: "UCLA",
-			status: "Student",
-			org: "UCLA Library"
-		},
-		{
-			name: "Will Foote",
-			blurb: "Ask me about statistics (and all things Wordle)!",
-			image: "../images/will.png",
-			affiliation: "UCLA",
-			status: "Student",
-			org: "UCLA Library"
 		},
 	]
 };
